773df56788ee715dd038da69f9b23e93dec4e9b6
JamesElliottMiller <James.Elliott.Miller@gmail.com>
2023-03-10 13:12:28 +0000

    Sets arguments

--- manual_rps.py
+++ manual_rps.py
@@ -32,12 +32,12 @@ def get_user_choice():
     else: 
      return(user_choice)
 
-def get_winner():
- user_choice = get_user_choice()
- computer_choice = get_computer_choice()
+def get_winner(user_choice, computer_choice):
  if user_choice == computer_choice:
     print("it's a tie")
  elif (computer_choice == "rock" and user_choice == "paper") or (computer_choice == "paper" and user_choice == "scissors") or (computer_choice == "scissors" and user_choice == "rock"):
     print("You Win")
  else:
     print("You Lose")
+
+get_winner(get_user_choice(), get_computer_choice())
